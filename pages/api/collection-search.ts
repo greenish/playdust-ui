9290784bc2c0bbce6751384788e2735794abbe07
Dustin Sanders <dustinsanders91@gmail.com>
2022-01-25 10:53:56 -0600

    Cr 361 - Add search API for ElasticSearch search capability in UI (#8)

---
+++ pages/api/collection-search.ts
@@ -0,0 +1,42 @@
+import type {
+  NextApiRequest,
+  NextApiResponse,
+} from 'next'
+import axios from 'axios'
+import esb from 'elastic-builder'
+
+const {
+  OPENSEARCH_USER,
+  OPENSEARCH_PASSWORD,
+  OPENSEARCH_URL,
+} = process.env
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const searchTerm = req.query.term as string
+  const requestBody = esb.requestBodySearch()
+    .query(esb.matchQuery('data.creators.address', searchTerm))
+    .collapse('data.creators.address')
+    .suggest(esb.termSuggester('nameSuggestor', 'data.name', searchTerm))
+
+  try {
+    const { data } = await axios.post(
+      `${OPENSEARCH_URL}/_search`,
+      requestBody.toJSON(),
+      {
+        auth: {
+          username: OPENSEARCH_USER as string,
+          password: OPENSEARCH_PASSWORD as string,
+        },
+      },
+    )
+
+    res.json(data)
+  } catch (e) {
+    res.json({ e })
+  }
+}
+
+export default handler
