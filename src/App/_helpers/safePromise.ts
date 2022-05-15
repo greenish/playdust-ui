6335d335c8a874d18550719e45f794abe0f12f5a
Philipp Adrian <hello@philippadrian.com>
2022-05-15 13:23:26 -0400

    Improve safePromise

--- src/App/_helpers/safePromise.ts
+++ src/App/_helpers/safePromise.ts
@@ -1,7 +1,16 @@
-function safePromise<T>(promise: Promise<T>) {
-  promise.catch((e) => {
+function safePromise<T>(
+  promise: Promise<T>,
+  passthrough: true
+): Promise<T | void>;
+function safePromise<T>(promise: Promise<T>, passthrough: false): void;
+function safePromise<T>(promise: Promise<T>): void;
+function safePromise<T>(promise: Promise<T>, passthrough?: boolean) {
+  const caught = promise.catch((e) => {
     console.error('SavePromise Caught', e);
   });
+  if (passthrough) {
+    return caught;
+  }
 }
 
 export default safePromise;
