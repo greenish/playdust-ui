ae22264269b37c9b05e616a918ba10b5f56d8299
React <ken_mccloskey@hotmail.com>
2022-05-22 12:07:33 -0400

    PD-774 - Create Config Account module(s) (#149)
    * Initial commit
    
    * Fixes types
    
    * Show all keys
    
    * Improve key/signer label

--- src/App/Window/WindowSwitch/Address/Address.tsx
+++ src/App/Window/WindowSwitch/Address/Address.tsx
@@ -5,8 +5,10 @@ import RawAccountData from './RawAccountData';
 import SPLTokenAccount from './SPLTokenAccount';
 import SPLTokenMint from './SPLTokenMint/SPLTokenMint';
 import SPLTokenMultisig from './SPLTokenMultisig';
+import StakeConfigAccountCard from './StakeConfigAccountCard';
 import TokenAccounts from './TokenAccounts/TokenAccounts';
 import Transactions from './Transactions/Transactions';
+import ValidatorInfoConfigAccountCard from './ValidatorInfoConfigAccountCard';
 import WalletGallery from './WalletGallery/WalletGallery';
 import ExplorerAccordion from './_sharedComponents/ExplorerAccordion';
 import SuspenseBoundary from './_sharedComponents/SuspenseBoundary/SuspenseBoundary';
@@ -27,6 +29,16 @@ function Address() {
         />
         <SuspenseBoundary
           content={<SPLTokenMultisig />}
+          error={null}
+          loading={null}
+        />
+        <SuspenseBoundary
+          content={<ValidatorInfoConfigAccountCard />}
+          error={null}
+          loading={null}
+        />
+        <SuspenseBoundary
+          content={<StakeConfigAccountCard />}
           error={null}
           loading={null}
         />
---
+++ src/App/Window/WindowSwitch/Address/ValidatorInfoConfigAccountCard.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import parsedConfigAccountAtom from './_atoms/parsedConfigAccountAtom';
+import ExplorerCard from './_sharedComponents/ExplorerCard';
+import ExplorerGrid from './_sharedComponents/ExplorerGrid';
+import ExplorerGridRow from './_sharedComponents/ExplorerGridRow';
+import LabeledAddressLink from './_sharedComponents/LabeledAddressLink/LabeledAddressLink';
+import TableSkeleton from './_sharedComponents/TableSkeleton/TableSkeleton';
+
+// 7qUt9itGTzT7QLNTbmoPwXCKR93pd4i8TqZp1BEj5ah8
+function ValidatorInfoConfigAccountCardRows() {
+  const parsedConfigAccount = useRecoilValue(parsedConfigAccountAtom);
+
+  if (!parsedConfigAccount || parsedConfigAccount.type !== 'validatorInfo') {
+    return null;
+  }
+
+  const { info } = parsedConfigAccount;
+
+  return (
+    <>
+      {info.configData.name && (
+        <ExplorerGridRow label="Name" value={info.configData.name} />
+      )}
+      {info.configData.keybaseUsername && (
+        <ExplorerGridRow
+          label="Keybase Username"
+          value={info.configData.keybaseUsername}
+        />
+      )}
+      {info.configData.website && (
+        <ExplorerGridRow label="Website" value={info.configData.website} />
+      )}
+      {info.configData.details && (
+        <ExplorerGridRow label="Details" value={info.configData.details} />
+      )}
+      {info.keys.map(({ pubkey, signer }, idx) => (
+        <ExplorerGridRow
+          label={`Key ${idx + 1}${signer ? ' (signer)' : ''}`}
+          value={<LabeledAddressLink to={pubkey} allowCopy={true} />}
+        />
+      ))}
+    </>
+  );
+}
+
+function ValidatorInfoConfigAccountCard() {
+  return (
+    <ExplorerCard loading={<TableSkeleton />} error={null}>
+      <ExplorerGrid>
+        <ValidatorInfoConfigAccountCardRows />
+      </ExplorerGrid>
+    </ExplorerCard>
+  );
+}
+
+export default ValidatorInfoConfigAccountCard;
