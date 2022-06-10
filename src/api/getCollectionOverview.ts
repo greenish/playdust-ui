1dd9e41796a2ec46f8e0dfe3dc2f4ee38d025789
Dustin Sanders <dustinsanders91@gmail.com>
2022-06-10 14:06:05 -0500

    yarn fix:all

--- src/api/getCollectionOverview.ts
+++ src/api/getCollectionOverview.ts
@@ -3,7 +3,7 @@ import {
   SearchRequest,
 } from '@opensearch-project/opensearch/api/types';
 import { string } from 'superstruct';
-import CollectionOverviewResponseType from '../App/Window/WindowSwitch/Address/SPLTokenMintNonFungible/MetaplexNFTCollectionOverview/_types/CollectionOverviewResponseType';
+import CollectionOverviewResponseType from '../App/Window/WindowSwitch/_types/CollectionOverviewResponseType';
 import type OpenSearchCollectionSourceType from '../App/Window/_types/OpenSearchCollectionSourceType';
 import getRarestNFTsByCollectionBody from './_helpers/getRarestNFTsByCollectionBody';
 import nextApiHandler from './_helpers/nextApiHandler';
