d76826a1cbbb8032445e59d57998cfddd75f5631
Philipp Adrian <hello@philippadrian.com>
2022-06-23 13:41:00 -0400

    yarn fix:all

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,33 @@
+import { datadogRum } from '@datadog/browser-rum';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React from 'react';
 import getPDEnv from '../src/App/_helpers/getPDEnv';
 import '../styles/globals.css';
-import { datadogRum } from '@datadog/browser-rum';
 
 datadogRum.init({
   applicationId: 'ec89a6b0-1b64-4a87-bf7a-2d19cdcb339d',
   clientToken: 'pubb1b51268c7e297f09c00ffbe8e387553',
   site: 'datadoghq.com',
-  service:'playdust',
-  env:getPDEnv(),
-  // Specify a version number to identify the deployed version of your application in Datadog 
+  service: 'playdust',
+  env: getPDEnv(),
+  // Specify a version number to identify the deployed version of your application in Datadog
   // version: '1.0.0',
   sampleRate: 100,
   premiumSampleRate: 100,
   trackInteractions: true,
-  defaultPrivacyLevel:'mask-user-input'
+  defaultPrivacyLevel: 'mask-user-input',
 });
-    
+
 datadogRum.startSessionReplayRecording();
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  
- 
+
   if (!router.isReady) {
     return null;
   }
-
-  
 
   return (
     <>
