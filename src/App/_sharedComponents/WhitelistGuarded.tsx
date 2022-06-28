d0fb5047415dad99fe4a66972a6ff66d36af03b8
Philipp Adrian <hello@philippadrian.com>
2022-06-28 13:44:35 -0400

    Fix / simplify whitelist logic

--- src/App/_sharedComponents/WhitelistGuarded.tsx
+++ src/App/_sharedComponents/WhitelistGuarded.tsx
@@ -8,23 +8,17 @@ function WhitelistGuarded({
 }: PropsWithChildren<{ fallback: NonNullable<ReactNode> | null }>) {
   const currentUserProfile = useRecoilValue(currentUserProfileAtom);
 
-  // When GO_LIVE is set to true we don't enforce whitelist so everyone has access.
-  // Setting GO_LIVE to true is also useful for local development.
-  const enforceWhitelist = process.env.GO_LIVE !== 'true';
+  const isWhitelisted =
+    process.env.WHITELIST_ACTIVE === 'true'
+      ? Boolean(currentUserProfile?.isWhitelisted)
+      : false;
+  const isAdmin = Boolean(currentUserProfile?.isAdmin);
 
-  if (enforceWhitelist) {
-    const userIsWhitelisted =
-      currentUserProfile &&
-      (currentUserProfile.isWhitelisted || currentUserProfile.isAdmin);
-
-    if (!userIsWhitelisted) {
-      // eslint-disable-next-line react/jsx-no-useless-fragment
-      return <>{fallback}</>;
-    }
+  if (process.env.GO_LIVE === 'true' || isAdmin || isWhitelisted) {
+    return children;
   }
 
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{children}</>;
+  return fallback;
 }
 
 export default WhitelistGuarded;
