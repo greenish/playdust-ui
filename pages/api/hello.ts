ba562aeb041ff226c94b042396ce4ca276cb181d
Dustin Sanders <dustinsanders91@gmail.com>
2022-07-29 09:02:12 -0500

    print OS url

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).send('Hello World');
+  res.status(200).json({
+    url: process.env.OPENSEARCH_URL,
+  });
 }
